30b0942f6341e1e6c99e285158e97153813a5cf5
pathmage <pathmage@outlook.com>
2024-12-03 13:39:18 +0100

    ParsableArrayAttribute

--- ToolKit/Utilities/Attributes/ParsableAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace ToolKit;
-
-[AttributeUsage(AttributeTargets.Field)]
-public sealed class ParsableAttribute<TParsable> : Attribute
-	where TParsable : IParsable<TParsable>;
---
+++ ToolKit/Utilities/Attributes/ParsableAttributes.cs
@@ -0,0 +1,9 @@
+﻿namespace ToolKit;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class ParsableAttribute<TParsable> : Attribute
+	where TParsable : IParsable<TParsable>;
+
+public sealed class ParsableArrayAttribute<TParsable>
+	: ParsableAttribute<TParsable>
+	where TParsable : IParsable<TParsable>;

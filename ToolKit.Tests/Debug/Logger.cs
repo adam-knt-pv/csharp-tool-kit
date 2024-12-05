51aad4c9f7e6d19eee6c4b3ca144cb4873c74808
pathmage <pathmage@outlook.com>
2024-12-05 16:04:20 +0100

    Constants

--- ToolKit.Tests/Debug/Logger.cs
+++ ToolKit.Tests/Debug/Logger.cs
@@ -6,6 +6,7 @@ public class Logger
 {
 	public static void ILogger()
 	{
+		printl(Constants.StringSet.TrueFalse);
 		print("Hello!");
 
 		print(null, "", typeof(Action<>));
---
+++ ToolKit/Utilities/Constants.cs
@@ -0,0 +1,54 @@
+﻿namespace ToolKit;
+
+public class Constants
+{
+	public static class CharSet
+	{
+		public static readonly char[] WhiteSpace = [' ', '\t'];
+
+		public static readonly char[] FileForbidden =
+		[
+			'/',
+			'\\',
+			'?',
+			'%',
+			'*',
+			':',
+			'|',
+			'"',
+			'<',
+			'>',
+			'.',
+			',',
+			';',
+			'=',
+			' ',
+			'\t',
+		];
+	}
+
+	public static class StringSet
+	{
+		/// <summary>
+		/// Array of all strings that mean true.
+		/// </summary>
+		public static readonly string[] True = ["true", "t", "yes", "y", "1"];
+
+		/// <summary>
+		/// Array of all strings that mean false.
+		/// </summary>
+		public static readonly string[] False = ["false", "f", "no", "n", "0"];
+
+		/// <summary>
+		/// Array of all strings that mean true and false.
+		/// </summary>
+		public static readonly string[] TrueFalse;
+
+		static StringSet()
+		{
+			TrueFalse = new string[True.Length + False.Length];
+			True.CopyTo(TrueFalse, 0);
+			False.CopyTo(TrueFalse, True.Length);
+		}
+	}
+}

811291b2dcb026bf7b3010aabb827621e28e803c
pathmage <pathmage@outlook.com>
2024-12-08 16:52:00 +0100

    BindingFlagsAllMembers constant

---
+++ pathmage.ToolKit/Utilities/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace pathmage.ToolKit;
+
+public static class Constants
+{
+	public static class Reflection
+	{
+		public const BindingFlags BindingFlagsAllMembers =
+			BindingFlags.Instance
+			| BindingFlags.Static
+			| BindingFlags.Public
+			| BindingFlags.NonPublic;
+	}
+}

1ce9dce619782a0729c26a9e433cee5724b4c6b0
pathmage <pathmage@outlook.com>
2025-01-27 21:54:53 +0100

    Enum extension

---
+++ pathmage.ToolKit/Extensions/System/Enum.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace pathmage.ToolKit.Extensions;
+
+partial class Extensions
+{
+	public static int ToInt32<TEnum>(this ref TEnum e)
+		where TEnum : struct, Enum => Unsafe.As<TEnum, int>(ref e);
+}
--- pathmage.ToolKit/ToolKit.cs
+++ pathmage.ToolKit/ToolKit.cs
@@ -6,8 +6,18 @@ global using pathmage.ToolKit.Globals;
 
 namespace pathmage.ToolKit
 {
+	namespace Collections { }
+
+	namespace Debug { }
+
 	namespace Extensions
 	{
 		public static partial class Extensions;
 	}
+
+	namespace Globals { }
+
+	namespace Helpers { }
+
+	namespace Utilities { }
 }
